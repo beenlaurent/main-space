edce0705aee6ed2aa0a3c01389856ec9598bf4d6
Laurent <laurentarcher3@gmail.com>
2022-05-03 18:20:46 -0400

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,19 @@
+#look back to this point once code finishes
+loop = 1
+while (loop < 10):
+    noun = input("Choose a noun: ")
+    p_noun = input("Choose a plural noun: ")
+    noun2 = input("Choose a noun: ")
+    place = input("Name a place: ")
+    adjective = input("Choose an adjective (Describing word): ")
+
+    noun3 = input("choose a noun: ")
+
+    print ("----------------------------------------------")
+    print ("be kind to your", noun, p_noun)
+    print("For a duck may be somebody's", noun2,".")
+    print("Be kind to your",p_noun,"in", place)
+    print ("Where the wether is always",adjective,"," )
+    print ("Well it is.")
+    print ("----------------------------------------------")
+    loop = loop + 1
